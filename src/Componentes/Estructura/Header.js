bdb9b4f3b602f7d75e6b30c2e017e04e46bab590
CarolinaIPD <75870831+CarolinaIPD@users.noreply.github.com>
2023-03-07 00:07:21 -0600

    panel derecho

--- src/Componentes/Estructura/Header.js
+++ src/Componentes/Estructura/Header.js
@@ -1,25 +1,49 @@
 import React from 'react';
 import '../../Estilos/header.css'
 import { logOut } from '../../Funciones/funciones';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
 const Header = ({isLogged}) => {
 
   return(
+    <section>
       <header className={isLogged ? 'background' : ''}>
-        <h2 className='logo'>SAGIT</h2>
+        <a className='logo' href='/'>SAGIT</a>
         <nav className='navigation'>
           {
             isLogged 
             ? <div>
-                <a href='/'>Home</a>
-                <button className='btnLogin-popup'>
+                <label for='btnFiltro' class="filtro" type="button">Filtros</label>
+                <label type="button" className='btnLogin-popup'>
                   <p className='expand' onClick={logOut} >Cerrar Sesión</p>
-                </button>
+                </label>
               </div>
             : <div></div>
           }
         </nav>
       </header>
+      <input type="checkbox" id="btnFiltro"/>
+      <div className='container-filtro'>
+        <div className='cont-filtro'>
+          <nav>
+            <form>
+              <label>Prueba 1</label>
+              <input type="text"></input>
+              <label>Prueba 2</label>
+              <input type="text"></input>
+              <label>Prueba 3</label>
+              <input type="text"></input>
+              <label>Prueba 4</label>
+              <input type="text"></input>
+              <button type='submit'>Filtrar</button>
+            </form>
+          </nav>
+          <label for='btnFiltro' className='closeFiltro'>C</label>
+        </div>
+      </div>
+    </section>
+      
   );
 };
 
