dcc8adb10041358c0551ba43496bd31fa8000d0e
CarolinaIPD <75870831+CarolinaIPD@users.noreply.github.com>
2023-03-06 21:47:59 -0600

    Fijación del head

--- src/Componentes/Layouts/Icon.js
+++ src/Componentes/Layouts/Icon.js
@@ -1,2 +1,2 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faLock,faEyeSlash} from '@fortawesome/free-solid-svg-icons';
